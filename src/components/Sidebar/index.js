cb9bbfc4273a3195db4662eb1ecf7214525318ce
miqueiasbelfort <kawasousa1554@gmail.com>
2022-08-01 16:16:19 -0300

    sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,61 @@
+import {
+    Container, 
+    MenuItem,
+    HomeIcon,
+    PeopleIcon, 
+    Following,
+    FollowingHeader,
+    InfoContainer,
+    Links
+} from "./styles"
+
+import User from "../User"
+
 function Sidebar(){
     return (
-        <div>
-            <h1>Sofia pro</h1>
-        </div>
+        <Container>
+            <MenuItem active>
+                <HomeIcon src="/images/homeIcon.svg"></HomeIcon>
+                <span>Para você</span>
+            </MenuItem>
+            <MenuItem>
+                <PeopleIcon src="/images/peopleIcon.svg"></PeopleIcon>
+                <span>Seguindo</span>
+            </MenuItem>
+            <Following>
+                <FollowingHeader>Suas principais contas</FollowingHeader>
+                <User 
+                    user={{
+                        name: "Will Smith",
+                        username: "willsmith",
+                        avatar: "https://p77-sign-va.tiktokcdn.com/musically-maliva-obj/1646315618666501~c5_100x100.jpeg?x-expires=1659463200&x-signature=XK5US0dPJLOG6m5F036L9KmkBb4%3D"
+                    }}
+                ></User>
+            </Following>
+            <InfoContainer>
+                <Links margin>
+                    <a>Inicio</a>
+                    <a>Sobre</a>
+                    <a>Sala de imprensa</a>
+                    <a>Carreira</a>
+                    <a>ByteDance</a>
+                </Links>
+                <Links>
+                    <a>Ajuda</a>
+                    <a>Segurança</a>
+                </Links>
+                <Links>
+                    <a>Diretrizes da comunidade</a>
+                    <a>Termos</a>
+                </Links>
+                <Links margin>
+                    <a>Privacidade</a>
+                </Links>
+                <Links margin>
+                    <a>@ 2022 tiktok</a>
+                </Links>
+            </InfoContainer>
+        </Container>
     )   
 }
 export default Sidebar
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layouts"
 function Index() {
     return (
         <Layout>
-        
+            Feed
         </Layout>
     )
 }
